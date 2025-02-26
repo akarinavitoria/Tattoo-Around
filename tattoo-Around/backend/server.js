3f0a0349afee44605af76290d5c9056d1e122e30
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-02-26 19:48:18 +0000

    .

--- tattoo-Around/backend/server.js
+++ tattoo-Around/backend/server.js
@@ -41,6 +41,6 @@ const errorHandler = require('./middlewares/errorMiddleware');
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`✅ Servidor rodando na porta ${PORT}`);
 });
