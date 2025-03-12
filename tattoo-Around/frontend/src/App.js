348df12dc1c611101b75bf5332c5f86e1a3eddb4
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-03-12 04:23:35 +0000

    .

--- tattoo-Around/frontend/src/App.js
+++ tattoo-Around/frontend/src/App.js
@@ -2,26 +2,12 @@
 import React from 'react';
 import { AuthProvider } from './context/AuthContext';
 import Routes from './Routes'; // Exemplo de arquivo de rotas
-import { HeaderWrapper, Title } from './components/Header';
-import Button from './components/Button';
 
 function App() {
   return (
     <AuthProvider>
       <Routes />
     </AuthProvider>
-  );
-
-  return (
-    <div>
-      <HeaderWrapper>
-        <Title>Tattoo Around</Title>
-      </HeaderWrapper>
-      <main style={{ padding: '1rem', textAlign: 'center' }}>
-        <Button primary>Entrar</Button>
-        <Button>Registrar</Button>
-      </main>
-    </div>
   );
 }
 
