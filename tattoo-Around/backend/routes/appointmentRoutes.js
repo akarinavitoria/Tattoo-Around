ce8b123633ade4011b0edae42452e9b24e30d7a7
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-03-12 03:31:21 +0000

    .

--- tattoo-Around/backend/routes/appointmentRoutes.js
+++ tattoo-Around/backend/routes/appointmentRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { protect, authorize } = require('../middlewares/authMiddleware'); // ✅ Corrigido
 const { createAppointment } = require('../controllers/appointmentController');
+const { cancelAppointment } = require('../controllers/appointmentController');
 const { body, validationResult } = require('express-validator');
 
 // Rota para criação de agendamentos
