050478ea716162268c1523f8e1374227130ca1ca
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-03-13 17:23:36 +0000

    .

--- tattoo-Around/frontend/src/cypress.config.js
+++ tattoo-Around/frontend/src/cypress.config.js
@@ -2,3 +2,15 @@ module.exports = {
     projectId: "41oeqm",
     // ...rest of the Cypress project config
   }
+
+  const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Implementar eventos do Node aqui, se necessário
+    },
+    baseUrl: "http://localhost:3000", // Altere para a URL do seu projeto
+    supportFile: "cypress/support/e2e.js",
+  },
+});
