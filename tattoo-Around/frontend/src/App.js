1af302596b49c50376f31d9bc61b0d9ad3ebf613
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-03-13 18:32:54 +0000

    .

--- tattoo-Around/frontend/src/App.js
+++ tattoo-Around/frontend/src/App.js
@@ -1,7 +1,17 @@
 // src/App.js
 import React from 'react';
+import Header from './components/Header/Header';
 import { AuthProvider } from './context/AuthContext';
 import Routes from './Routes'; // Exemplo de arquivo de rotas
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes />
+    </div>
+  );
+}
 
 function App() {
   return (
