30d89fb4a5e983f5dc7c584491f04d428027ca98
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-03-13 18:28:07 +0000

    .

--- tattoo-Around/frontend/cypress/e2e/sample.cy.js
+++ tattoo-Around/frontend/cypress/e2e/sample.cy.js
@@ -1,7 +1,7 @@
 describe('Página inicial', () => {
     it('deve carregar corretamente', () => {
       cy.visit('/');
-      cy.contains('TattooAround').should('be.visible');
+      cy.contains('Tattoo Around').should('be.visible');
       // Altere conforme necessário
     });
   });
