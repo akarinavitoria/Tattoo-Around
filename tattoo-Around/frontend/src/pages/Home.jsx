2171014efa5e08cb9f675a0739c1837ec90595e9
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-03-14 16:44:26 +0000

    .

---
+++ tattoo-Around/frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Bem-vindo ao Tattoo Around</h2>
+      <p>Esta é a página inicial.</p>
+    </div>
+  );
+};
+
+export default Home;
