9cd7b148aaf19835137d48a6eccbb9aefecd243b
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-03-17 02:16:37 +0000

    .

--- tattoo-Around/frontend/src/App.js
+++ tattoo-Around/frontend/src/App.js
@@ -1,24 +1,23 @@
-import { BrowserRouter } from "react-router-dom"
-import { HeaderWrapper, Title } from "./components/Header/Header" // Importação correta dos componentes nomeados
-import { AuthProvider } from "./context/AuthContext"
-import Routes from "./Routes" // Ajuste para o nome correto do seu arquivo de rotas
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/Header"; // Importa o componente default
+import { AuthProvider } from "./context/AuthContext";
+import Routes from "./Routes";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <div className="app-container">
-          <HeaderWrapper>
-            <Title>Tattoo Around</Title>
-          </HeaderWrapper>
+          <Header />
           <main>
             <Routes />
           </main>
         </div>
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
+
 
