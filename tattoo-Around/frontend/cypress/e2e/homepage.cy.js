bfe9b866fd3957b6b8adb6cb72df28722b79eb40
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-03-17 02:30:07 +0000

    .

--- tattoo-Around/frontend/cypress/e2e/homepage.cy.js
+++ tattoo-Around/frontend/cypress/e2e/homepage.cy.js
@@ -1,7 +1,7 @@
 describe('Página Inicial', () => {
     it('deve exibir o logo no Header', () => {
       // Visita a página inicial, usando a baseUrl definida em cypress.config.js (por exemplo, http://localhost:3000)
-      cy.visit('/');
+      cy.visit('http://localhost:3000');
   
       // Verifica se o elemento com a classe "logo" contém o texto "Tattoo Around"
       cy.get('logo', { timeout: 15000 }).should('contain.text', 'Tattoo Around');
