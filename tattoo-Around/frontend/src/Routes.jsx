bf14d832ad6db52ded56ea84096346ef1f2b070c
Karina Vitória <165606513+akarinavitoria@users.noreply.github.com>
2025-03-18 17:44:08 +0000

    .

--- tattoo-Around/frontend/src/Routes.jsx
+++ tattoo-Around/frontend/src/Routes.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Signup from "./pages/Signup";
 import Login from './pages/Login';
 import ArtistProfile from './pages/ArtistProfile';
 import Profile from './pages/Profile';
@@ -12,6 +13,7 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/artist-profile" element={<ArtistProfile />} />
       <Route path="/profile" element={<Profile />} />
